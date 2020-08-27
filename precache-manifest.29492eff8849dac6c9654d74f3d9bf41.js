aa43ae25858e4414396aa3e781e16a282eff39ae
LikeDreamwalker <45194556+LikeDreamwalker@users.noreply.github.com>
2020-08-27 20:41:56 +0800

    setup

---
+++ precache-manifest.29492eff8849dac6c9654d74f3d9bf41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df75ca9fd6dcf3e11718",
+    "url": "/css/app.6bd8b95b.css"
+  },
+  {
+    "revision": "f08dc1ee76af41272d34",
+    "url": "/css/chunk-vendors.27ca7b4d.css"
+  },
+  {
+    "revision": "d818abb0a95d068c4bbfff102f6c7dbd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df75ca9fd6dcf3e11718",
+    "url": "/js/app.5f92f30b.js"
+  },
+  {
+    "revision": "f08dc1ee76af41272d34",
+    "url": "/js/chunk-vendors.c6717b30.js"
+  },
+  {
+    "revision": "6f621280d89b5ef2dfeaf4ef125a9244",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
